fff29bde725b5895070195093cc4c181104a1f51
kshitiz <kshtzkr@gmail.com>
2017-08-31 00:02:25 +0530

    add description to project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
 	end
 	
 	def user_params
-		params.require(:user).permit(:email,:avatar)
+		params.require(:user).permit(:email,:avatar,:description)
 	end
 end
